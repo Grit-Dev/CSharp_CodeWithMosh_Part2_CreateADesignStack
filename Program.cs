3e3525f82f4368d319b266ada49e958b85a7bbc6
JavHalla <73223185+Paulmcginley93@users.noreply.github.com>
2021-10-26 13:07:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+
+    /*
+     * The Push() method stores the given object on top of the stack. We use the “object” type here so we can store 
+     * any objects inside the stack. Remember the “object” class is the base of all classes in the .NET Framework. 
+     * So any types can be automatically upcast to the object. Make sure to take into account the scenario that null 
+     * is passed to this object. We should not store null references in the stack. So if null is passed to this
+     * method, you should throw an InvalidOperationException. Remember, when coding every method, you should think of
+     * all possibilities and make sure the method behaves properly in all these edge cases. That’s what distinguishes
+     * you from an “average” programmer. 
+     * 
+     * The Pop() method removes the object on top of the stack and returns it. Make sure to take into account 
+     * the scenario that we call the Pop() method on an empty stack. In this case, 
+     * this method should throw an InvalidOperationException. Remember, your classes should always be in a valid 
+     * state and used properly. When they are misused, they should throw exceptions. Again, thinking of all these 
+     * edge cases, separates you from an average programmer. The code written this way will be more robust and with 
+     * less bugs. 
+     * 
+     * The Clear() method removes all objects from the stack. 
+     */
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+
+
+
+
+        }
+    }
+    
+}
---
+++ Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp5
+{
+    public class Stack : object
+    {
+        private object object1;
+
+        private readonly ArrayList list = new ArrayList();
+
+        public void Push(Object obj)
+        {
+            if (obj == null)
+            {
+                Console.WriteLine("You cannot put nothing in the stack \n");
+
+            }
+            else
+            {
+                list.Add(obj);
+            }
+
+        }
+
+        public object Pop()
+        {
+            if (list.Count != 0)
+            {
+                object1 = list[list.Count - 1];
+                list.Remove(object1);
+
+                //Note:
+                //
+                //To understand this: 123 is stored in the List
+                //The list looks in to see what is in the length of the array list which would
+                //be position [4] -1. Which is 3.
+                //3 is moved from the array list and then
+                // returned to be displayed
+
+                //Once the method is called again
+                //it will find the length - 2. which is 2 and so on
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is already empty. Nothing to remove. ");
+            }
+            return object1;
+        }
+
+        public void Clear()
+        {
+            if (list.Count != 0)
+            {
+                list.Clear();
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is already cleared");
+            }
+
+        }
+    }
+    
+}
